d037a47d77ee1c25be6842e983568b26da4b2818
João Paulo <joaowais@gmail.com>
2024-08-25 20:44:46 -0300

    Consultando objetos do banco de dados

---
+++ src/main/java/com/example/food_api/jpa/CadastroCozinha.java
@@ -0,0 +1,19 @@
+package com.example.food_api.jpa;
+
+import com.example.food_api.domain.model.Cozinha;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    public List<Cozinha> listar() {
+        return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+    }
+}
