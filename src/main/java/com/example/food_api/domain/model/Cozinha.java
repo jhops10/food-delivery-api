58dc6e00ba35ea952f521a24aa4b2529aaf35859
João Paulo <joaowais@gmail.com>
2024-08-26 16:04:52 -0300

    Conhecendo e usando o Lombok

--- src/main/java/com/example/food_api/domain/model/Cozinha.java
+++ src/main/java/com/example/food_api/domain/model/Cozinha.java
@@ -1,12 +1,16 @@
 package com.example.food_api.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -14,33 +18,4 @@ public class Cozinha {
     @Column
     private String nome;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return Objects.equals(getId(), cozinha.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getId());
-    }
 }
