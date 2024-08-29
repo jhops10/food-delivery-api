aea20be841ab4f382ec603f335f9052bfdd15064
João Paulo <joaowais@gmail.com>
2024-08-28 22:13:28 -0300

    Modelando e requisitando um Collection Resource com GET

---
+++ src/main/java/com/example/food_api/api/controller/CozinhaController.java
@@ -0,0 +1,23 @@
+package com.example.food_api.api.controller;
+
+import com.example.food_api.domain.model.Cozinha;
+import com.example.food_api.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}
