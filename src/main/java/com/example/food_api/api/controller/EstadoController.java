aba1f24172bd62e8881e221b4a90ad7887296a8d
João Paulo <joaowais@gmail.com>
2024-08-29 12:37:46 -0300

    Collection Resource de Estados

---
+++ src/main/java/com/example/food_api/api/controller/EstadoController.java
@@ -0,0 +1,23 @@
+package com.example.food_api.api.controller;
+
+import com.example.food_api.domain.model.Estado;
+import com.example.food_api.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/estados")
+public class EstadoController {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @GetMapping
+    public List<Estado> listar() {
+        return estadoRepository.listar();
+    }
+}
