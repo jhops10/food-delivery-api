991a93d259d1d6fa8af0bf2e5b187a55ac208968
João Paulo <joaowais@gmail.com>
2024-08-29 15:45:01 -0300

    Consultando Singleton Resource com GET e PathVariable

--- src/main/java/com/example/food_api/api/controller/CozinhaController.java
+++ src/main/java/com/example/food_api/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import com.example.food_api.domain.model.Cozinha;
 import com.example.food_api.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id) {
+        return cozinhaRepository.buscar(id);
     }
 }
