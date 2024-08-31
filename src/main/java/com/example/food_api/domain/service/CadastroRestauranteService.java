96ee4b9abbb58e475e6f25003bb187264978e1f1
João Paulo <joaowais@gmail.com>
2024-08-31 19:35:03 -0300

    Modelando e implementando a inclusão de recursos de restaurantes

---
+++ src/main/java/com/example/food_api/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,31 @@
+package com.example.food_api.domain.service;
+
+import com.example.food_api.domain.exception.EntidadeNaoEncontradaException;
+import com.example.food_api.domain.model.Cozinha;
+import com.example.food_api.domain.model.Restaurante;
+import com.example.food_api.domain.repository.CozinhaRepository;
+import com.example.food_api.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroRestauranteService {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public Restaurante salvar(Restaurante restaurante) {
+        Long cozinhaId = restaurante.getCozinha().getId();
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinha == null) {
+            throw new EntidadeNaoEncontradaException(String.format("Não existe cadastro de cozinha com o código %d", cozinhaId));
+        }
+
+        restaurante.setCozinha(cozinha);
+        return restauranteRepository.salvar(restaurante);
+    }
+}
