1c3b92a23676c9f817fcf89677ef9eb377916763
João Paulo <joaowais@gmail.com>
2024-09-05 13:05:50 -0300

    Implementando consultas JPQL em repositórios

---
+++ src/main/java/com/example/food_api/api/controller/TesteController.java
@@ -0,0 +1,24 @@
+package com.example.food_api.api.controller;
+
+import com.example.food_api.domain.model.Cozinha;
+import com.example.food_api.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/teste")
+public class TesteController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome (@RequestParam("nome") String nome) {
+        return cozinhaRepository.consultarPorNome(nome);
+    }
+}
