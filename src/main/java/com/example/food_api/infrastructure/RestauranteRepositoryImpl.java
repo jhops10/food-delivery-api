8228c285a00372dd0e511366d4adf48cf9078c52
João Paulo <joaowais@gmail.com>
2024-09-09 14:12:26 -0300

    Implementando uma consulta simples com Criteria API

--- src/main/java/com/example/food_api/infrastructure/RestauranteRepositoryImpl.java
+++ src/main/java/com/example/food_api/infrastructure/RestauranteRepositoryImpl.java
@@ -5,6 +5,8 @@ import com.example.food_api.domain.repository.RestauranteRepositoryQueries;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -20,31 +22,15 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-        var jpql = new StringBuilder();
-        jpql.append("from Restaurante where 0 = 0 ");
 
-        var parametros = new HashMap<String, Object>();
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
 
-        if (StringUtils.hasLength(nome)) {
-            jpql.append("and nome like :nome ");
-            parametros.put("nome", "%" + nome + "%");
-        }
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 
-        if (taxaFreteInicial != null) {
-            jpql.append("and taxaFrete >= :taxaInicial ");
-            parametros.put("taxaInicial", taxaFreteInicial);
-        }
+        criteria.from(Restaurante.class);
 
-        if (taxaFreteFinal != null) {
-            jpql.append("and taxaFrete <= :taxaFinal ");
-            parametros.put("taxaFinal", taxaFreteFinal);
-        }
 
-        TypedQuery<Restaurante> query = manager
-                .createQuery(jpql.toString(), Restaurante.class);
-
-        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-
+        TypedQuery<Restaurante> query =  manager.createQuery(criteria);
         return query.getResultList();
     }
 }
