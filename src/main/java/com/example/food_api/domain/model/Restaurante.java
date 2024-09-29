fe42a08ef4736b80b9e0f72c03034f962fbebf27
João Paulo <joaowais@gmail.com>
2024-09-29 16:59:42 -0300

    Resolvendo o Problema do N+1 com fetch join na JPQL

--- src/main/java/com/example/food_api/domain/model/Restaurante.java
+++ src/main/java/com/example/food_api/domain/model/Restaurante.java
@@ -1,7 +1,6 @@
 package com.example.food_api.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
